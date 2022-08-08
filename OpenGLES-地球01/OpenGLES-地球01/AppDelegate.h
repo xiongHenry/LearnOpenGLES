80071542fe415c8e13ed97f67393536f4ca491ca
xiongtao <xiongtao_henry@163.com>
2022-08-08 20:13:11 +0800

    update

---
+++ OpenGLES-地球01/OpenGLES-地球01/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OpenGLES-地球01
+//
+//  Created by 熊涛 on 2022/8/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OpenGLES-地球01/OpenGLES-地球01/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  OpenGLES-地球01
+//
+//  Created by 熊涛 on 2022/8/8.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+
+@end
+
